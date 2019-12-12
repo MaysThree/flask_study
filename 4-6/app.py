1b1b4ac7d463e92e1857bcb90748233b1ff4bd9b
MaysThree <MaysThree@github.com>
2019-12-12 17:20:49 +0800

    使用wrap优化装饰器的函数

---
+++ 4-6/app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from functools import wraps
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+def user_login(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        print('登录操作')
+        func(*args, **kwargs)
+    return inner
+
+
+@user_login
+def news():
+    print(news.__name__)
+
+
+news()
+
+
+@user_login
+def news_list(*args):
+    page = args[0]
+    print(news_list.__name__)
+    print('这是新闻第 %s 页!' % page)
+
+
+news_list(5)
+
+
+if __name__ == '__main__':
+    app.run()
