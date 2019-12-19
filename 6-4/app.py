d82deb86cc7c0d9cbd94412af7d416c5edbf41c3
MaysThree <MaysThree@github.com>
2019-12-19 09:45:17 +0800

    flask-sqlalchemy模型与表映射,增删改查

--- 6-4/app.py
+++ 6-4/app.py
@@ -20,13 +20,55 @@ class Book(db.Model):
 
 
 # db.create_all()
-book1 = Book(id='001', title='flask入门-钱游', publishing_office='forget', isbn='12465789')
-try:
-    db.session.add(book1)
+# book1 = Book(id='001', title='flask入门-钱游', publishing_office='forget', isbn='12465789')
+# try:
+#     db.session.add(book1)
+#     db.session.commit()
+# except Exception as e:
+#     print("eeerro")
+#     print(e)
+
+
+@app.route('/add')
+def add_book():
+    book1 = Book(title='flask入门-钱游-1', publishing_office='forget1', isbn='124657891')
+    book2 = Book(title='flask入门-钱游-2', publishing_office='forget2', isbn='124657892')
+    book3 = Book(title='flask入门-钱游-3', publishing_office='forget3', isbn='124657893')
+    try:
+        db.session.add(book1)
+        db.session.add(book2)
+        db.session.add(book3)
+        db.session.commit()
+        return '添加成功'
+    except Exception as e:
+        print(e)
+        return '添加失败'
+
+
+@app.route('/select')
+def select():
+    result = Book.query.filter(Book.publishing_office == 'forget1').all()
+    print(result)
+    for books in result:
+        print(books.title)
+    return 'query success!'
+
+
+@app.route('/edit')
+def edit():
+    result = Book.query.filter(Book.id == '1').first()
+    print(result)
+    result.publishing_office = 'forget again'
     db.session.commit()
-except Exception as e:
-    print("eeerro")
-    print(e)
+    return 'edit success!'
+
+
+@app.route('/delete')
+def delete():
+    result = Book.query.filter(Book.id == '10').first()
+    db.session.delete(result)
+    db.session.commit()
+    return 'delete success!'
 
 
 @app.route('/')
@@ -35,4 +77,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
