b867ee6edcdf5ba23a125f7d4ec2eabfdcf2f669
MaysThree <MaysThree@github.com>
2019-12-23 15:55:17 +0800

    避免循环引用问题

---
+++ 6-15/external.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+USERNAME = 'root'
+PASSWORD = 'root'
+HOST = '127.0.0.1'
+PORT = 3306
+DATABASE = 'demo_01'
+DB_URI = "mysql://{}:{}@{}:{}/{}?charset=utf8". \
+        format(USERNAME, PASSWORD, HOST, PORT, DATABASE)
+SQLALCHEMY_DATABASE_URI = DB_URI
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
